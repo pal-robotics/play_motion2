890e00aebff7b5f2a739e2a11b659906deaa312f
davidfernandez <david.fernandez@pal-robotics.com>
2025-05-27 14:13:09 +0200

    Move Python client to PlayMotion2 pkg

--- play_motion2/test/test_client.py
+++ play_motion2/test/test_client.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2025 PAL Robotics S.L. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import time
 import unittest
 from threading import Thread
@@ -12,7 +26,7 @@ from play_motion2_msgs.action import PlayMotion2
 from play_motion2_msgs.srv import ListMotions, IsMotionReady, GetMotionInfo, \
     AddMotion, RemoveMotion
 
-from play_motion2_client_py import PlayMotion2ClientPy
+from play_motion2 import PlayMotion2ClientPy
 
 
 class ClientTest(unittest.TestCase):
